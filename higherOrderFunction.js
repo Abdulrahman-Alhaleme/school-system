91bee4e40a2b47e9c20650cdfb146fe4031587a2
Abdulrahman-Alhaleme <abdulrahman.alhaleme@gmail.com>
2023-03-26 21:43:30 -0700

    update

--- higherOrderFunction.js
+++ higherOrderFunction.js
@@ -2,7 +2,7 @@
 // 5- Write a function that, takes an array of integers as input, iterates over the array, and returns a new array.
 // The returned array should contain the result of raising 2 to the power of the original input element.
 // For example,([1,2,3]) returns [2,4,8] because 2 ^ 1 = 2, 2 ^ 2 = 4, and 2 ^ 3 = 8.
-// Use for and forEach and then map to solve this question to see the difference between the three ways 
+// Use for and forEach and then map to solve this question to see the difference between the three ways
 
 let arrQ5 = [1, 2, 3]
 
